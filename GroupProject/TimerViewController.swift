2a5b0991b84e9bd0937e21ff1fe38f60db4b2e98
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-12-18 21:25:25 +0200

    Updated connection with TimeViewModel

--- GroupProject/TimerViewController.swift
+++ GroupProject/TimerViewController.swift
@@ -1,27 +1,26 @@
 import UIKit
 import SnapKit
+import Combine
 
 class TimerViewController: UIViewController {
-    
     private let timerLabel = UILabel()
     private let startButton = UIButton(type: .system)
     private let pauseButton = UIButton(type: .system)
     private let stopButton = UIButton(type: .system)
     private let modeLabel = UILabel()
-    
-    private let pomodoroTimer = PomodoroTimer()
-    
+
+    private let timerViewModel = TimerViewModel(timer: PomodoroTimer())
+    private var cancellables = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        configureTimerCallbacks()
+        bindViewModel()
         hideKeyboardWhenTappedAround()
     }
-    
     private func setupUI() {
         view.backgroundColor = .white
         navigationItem.title = "Pomodoro Timer"
-        
         timerLabel.text = "25:00"
         timerLabel.font = UIFont.systemFont(ofSize: 48, weight: .bold)
         timerLabel.textAlignment = .center
@@ -30,7 +29,6 @@ class TimerViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
             make.centerX.equalToSuperview()
         }
-        
         modeLabel.text = "Work"
         modeLabel.font = UIFont.systemFont(ofSize: 20, weight: .medium)
         modeLabel.textAlignment = .center
@@ -39,28 +37,29 @@ class TimerViewController: UIViewController {
             make.top.equalTo(timerLabel.snp.bottom).offset(20)
             make.centerX.equalToSuperview()
         }
-        
+
+        setupButtons()
+    }
+
+    private func setupButtons() {
         startButton.setTitle("Start", for: .normal)
         startButton.setTitleColor(.white, for: .normal)
         startButton.backgroundColor = .systemGreen
         startButton.layer.cornerRadius = 8
         startButton.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
-        view.addSubview(startButton)
-        
+
         pauseButton.setTitle("Pause", for: .normal)
         pauseButton.setTitleColor(.white, for: .normal)
         pauseButton.backgroundColor = .systemOrange
         pauseButton.layer.cornerRadius = 8
         pauseButton.addTarget(self, action: #selector(pauseTimer), for: .touchUpInside)
-        view.addSubview(pauseButton)
-        
+
         stopButton.setTitle("Stop", for: .normal)
         stopButton.setTitleColor(.white, for: .normal)
         stopButton.backgroundColor = .systemRed
         stopButton.layer.cornerRadius = 8
         stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
-        view.addSubview(stopButton)
-        
+
         let buttonStack = UIStackView(arrangedSubviews: [startButton, pauseButton, stopButton])
         buttonStack.axis = .horizontal
         buttonStack.spacing = 20
@@ -72,32 +71,28 @@ class TimerViewController: UIViewController {
             make.height.equalTo(50)
         }
     }
-    
-    private func configureTimerCallbacks() {
-        pomodoroTimer.onTick = { [weak self] formattedTime in
-            self?.timerLabel.text = formattedTime
-        }
-        
-        pomodoroTimer.onModeChange = { [weak self] modeDescription in
-            self?.modeLabel.text = modeDescription.contains("Work") ? "Work" : "Break"
-        }
-        
-        pomodoroTimer.onStreakUpdate = { [weak self] currentStreak, overallStreak in
-            print("Current Streak: \(currentStreak), Overall Streak: \(overallStreak)")
-        }
+
+    private func bindViewModel() {
+        timerViewModel.$timerDisplay
+            .sink { [weak self] display in
+                self?.timerLabel.text = display
+            }
+            .store(in: &cancellables)
+
+        timerViewModel.$currentMode
+            .sink { [weak self] mode in
+                self?.modeLabel.text = mode == .work ? "Work" : "Break"
+            }
+            .store(in: &cancellables)
     }
-    
     @objc private func startTimer() {
-        pomodoroTimer.start()
+        timerViewModel.start()
     }
-    
     @objc private func pauseTimer() {
-        pomodoroTimer.pause()
+        timerViewModel.pause()
     }
-    
     @objc private func stopTimer() {
-        pomodoroTimer.stop()
-        timerLabel.text = "25:00"
+        timerViewModel.stop()
         modeLabel.text = "Work"
     }
 }
